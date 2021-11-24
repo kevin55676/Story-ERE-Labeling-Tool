b93254b179a6deb670f1c85d4d5bccd8b1015609
Joshua <joshua87719@gmail.com>
2021-11-25 01:23:43 +0800

    新增script.js與按鈕監聽事件

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Story-ERE-Labeling-Tool
+
+## Some useful development links
+
+[3小時初學者教學](https://youtu.be/yZwlW5INhgk)
+[bootstrap](https://bootstrap5.hexschool.com/docs/5.0/getting-started/introduction/)
---
+++ script.js
@@ -0,0 +1,13 @@
+var btn_new = document.getElementById('new_btn');
+var btn_commit = document.getElementById('commit_btn');
+
+function click_new() {
+    alert('叫你按就按阿?');
+}
+
+function click_commit() {
+    alert('按你媽逼');
+}
+
+btn_new.addEventListener('click', click_new)
+btn_commit.addEventListener('click', click_commit)
