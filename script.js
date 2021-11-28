6e9ecbebe079b5d8d4e51c8f27afacfd44b08f37
Joshua <joshua87719@gmail.com>
2021-11-28 14:32:18 +0800

    Get file list from server

--- script.js
+++ script.js
@@ -9,5 +9,34 @@ function click_commit() {
     alert('按你媽逼');
 }
 
-btn_new.addEventListener('click', click_new)
-btn_commit.addEventListener('click', click_commit)
+btn_new.addEventListener('click', click_new);
+btn_commit.addEventListener('click', click_commit);
+
+var file_selector = document.getElementById('file_selector');
+
+var stories_json;
+
+function get_story_data() {
+    var url = 'http://140.115.54.59:8000/StoryList';
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            // console.log(myJson);
+            stories_json = myJson;
+            // console.log(stories_json);
+        });
+}
+
+function get_file_list() {
+    // for (story in stories_json) {
+    //     file_selector.append(story)
+    // }
+    file_selector.append("testtets");
+}
+
+get_story_data();
+
+
+get_story_data();
