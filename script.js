6dad724760845dcb99a018a08201ebc8a5941f40
BuildTools <unconfigured@null.spigotmc.org>
2021-11-30 01:01:59 +0800

    complete tokenization

--- script.js
+++ script.js
@@ -50,18 +50,31 @@ function display_story(f_title, f_cont) {
 
     title.textContent = f_title;
     content.textContent = f_cont;
+    // var tok_list = tokenization(f_cont);
+    // doc2btn(tok_list);
 }
 
 function tokenization(string) {
+    var exp = new RegExp('([\u4e00-\u9fa5])');
+    var ret_list = [];
+    var splited = string.split(exp);
+    for (var tok = 0; tok < splited.length; tok++) {
+        if (splited[tok].trim().length > 0) {
+            ret_list.push(splited[tok].trim());
+        }
+    }
 
+    // console.log(ret_list);
+    return ret_list;
+}
+
+function doc2btn(tok_list) {
+    for (var i = 0; i < tok_list.length; i++) {
+        var btn = document.createElement("button");
+        btn.innerHTML = tok_list[i];
+        var content = document.getElementById('card_content');
+        content.appendChild(btn);
+    }
 }
 
 get_story_data();
-
-function regTest() {
-    var regExp = new RegExp('a-zA-Z0-9');
-    var string = 'this is test'
-    console.log(string.split(regExp));
-}
-
-regTest();
