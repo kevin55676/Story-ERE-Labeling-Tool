016e37ce67dcd97a29b871dd77286c3bd8b147b6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 00:31:21 +0800

    新增 label_meun_1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var btn_new = document.getElementById('new_btn');
 var btn_commit = document.getElementById('commit_btn');
 var file_selector = document.getElementById('file_selector');
+
+var tab_name = 'Entity';
 
 
 function click_new() {
@@ -12,32 +13,53 @@ function click_commit() {
 }
 
 function file_selected() {
-    var val = file_selector.value;
+    let val = file_selector.value;
     display_story(stories_json[val].file_name, stories_json[val].file_content);
 }
 
-btn_new.addEventListener('click', click_new);
+function change_tab(btn) {
+    let label_menu_entity = document.getElementById('label_menu_entity');
+    let label_menu_relation = document.getElementById('label_menu_relation');
+    let label_menu_event = document.getElementById('label_menu_event');
+
+    let menus = [label_menu_entity, label_menu_relation, label_menu_event];
+    console.log(btn.dataset.value);
+    console.log(menus[2].getAttribute("data-value"));
+    console.log(menus[2].getAttribute("data-value") == btn.dataset.value);
+
+
+    for (let i = 0; i < menus.length; i++) {
+        if (menus[i].getAttribute("data-value") == btn.dataset.value) {
+            menus[i].style.display = 'block';
+        } else {
+            menus[i].style.display = 'none';
+        }
+    }
+
+    tab_name = btn.innerHTML;
+    // console.log(tab_name);
+}
+
 btn_commit.addEventListener('click', click_commit);
 file_selector.addEventListener('change', file_selected);
 
 var stories_json;
 
 function get_story_data() {
-    var url = 'http://140.115.54.59:8000/StoryList';
+    let url = 'http://140.115.54.59:8000/StoryList';
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (myJson) {
             stories_json = myJson;
-            // console.log(stories_json);
             get_file_list();
         });
 }
 
 function get_file_list() {
-    for (var i = 0; i < stories_json.length; i++) {
-        var option = document.createElement('option');
+    for (let i = 0; i < stories_json.length; i++) {
+        let option = document.createElement('option');
         option.text = stories_json[i].file_name;
         option.value = i;
         file_selector.appendChild(option);
@@ -45,8 +67,8 @@ function get_file_list() {
 }
 
 function display_story(f_title, f_cont) {
-    var title = document.getElementById('card_title');
-    var content = document.getElementById('card_content')
+    let title = document.getElementById('card_title');
+    let content = document.getElementById('card_content')
 
     title.textContent = f_title;
     content.textContent = f_cont;
@@ -55,24 +77,23 @@ function display_story(f_title, f_cont) {
 }
 
 function tokenization(string) {
-    var exp = new RegExp('([\u4e00-\u9fa5])');
-    var ret_list = [];
-    var splited = string.split(exp);
-    for (var tok = 0; tok < splited.length; tok++) {
+    let exp = new RegExp('([\u4e00-\u9fa5])');
+    let ret_list = [];
+    let splited = string.split(exp);
+    for (let tok = 0; tok < splited.length; tok++) {
         if (splited[tok].trim().length > 0) {
             ret_list.push(splited[tok].trim());
         }
     }
 
-    // console.log(ret_list);
     return ret_list;
 }
 
 function doc2btn(tok_list) {
-    for (var i = 0; i < tok_list.length; i++) {
-        var btn = document.createElement("button");
+    for (let i = 0; i < tok_list.length; i++) {
+        let btn = document.createElement("button");
         btn.innerHTML = tok_list[i];
-        var content = document.getElementById('card_content');
+        let content = document.getElementById('card_content');
         content.appendChild(btn);
     }
 }
