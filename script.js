b6ce88d0a4cda1514b757c5003693ccfa37521d5
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 20:51:03 +0800

    改變tagging options的呈現方式

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var btn_commit = document.getElementById('commit_btn');
 var file_selector = document.getElementById('file_selector');
+var select1 = document.getElementById('select_1');
+var select2 = document.getElementById('select_2');
+var select3 = document.getElementById('select_3');
+select2.style.display = 'none';
+select3.style.display = 'none';
 
-var tab_name = 'Entity';
-
-
-function click_new() {
-    alert('叫你按就按阿?');
-}
+var current_tab = 'Entity';
+var current_menu1 = '';
+var current_menu2 = '';
+var current_menu3 = '';
 
 function click_commit() {
-    alert('按你媽逼');
+    alert('Commit to Server!');
 }
 
 function file_selected() {
@@ -17,31 +20,82 @@ function file_selected() {
     display_story(stories_json[val].file_name, stories_json[val].file_content);
 }
 
-function change_tab(btn) {
-    let label_menu_entity = document.getElementById('label_menu_entity');
-    let label_menu_relation = document.getElementById('label_menu_relation');
-    let label_menu_event = document.getElementById('label_menu_event');
+function remove_all_options() {
+    while (select1.firstChild) {
+        select1.removeChild(select1.firstChild);
+    }
+}
 
-    let menus = [label_menu_entity, label_menu_relation, label_menu_event];
-    console.log(btn.dataset.value);
-    console.log(menus[2].getAttribute("data-value"));
-    console.log(menus[2].getAttribute("data-value") == btn.dataset.value);
+function tagging_selections_reset() {
+    while (select1.firstChild) {
+        select1.removeChild(select1.firstChild);
+    }
+    select2.style.display = 'none';
+    select3.style.display = 'none';
+}
 
+function generate_options_1(tab) {
+    let select1_opts = ERE_menu1[tab];
 
-    for (let i = 0; i < menus.length; i++) {
-        if (menus[i].getAttribute("data-value") == btn.dataset.value) {
-            menus[i].style.display = 'block';
-        } else {
-            menus[i].style.display = 'none';
+    for (let i = 0; i < select1_opts.length; i++) {
+        let option = document.createElement('option');
+        option.text = select1_opts[i];
+        option.value = select1_opts[i];
+        select1.appendChild(option);
+    }
+}
+
+function select1_changed() {
+    let opt_value = select1.value;
+    let select2_opts = menu1_menu2[opt_value];
+    if (typeof select2_opts == "undefined") {
+        select2.style.display = 'none';
+        select3.style.display = 'none';
+    } else {
+        select2.style.display = 'block';
+        while (select2.firstChild) {
+            select2.removeChild(select2.firstChild);
+        }
+        for (let i = 0; i < select2_opts.length; i++) {
+            let option = document.createElement('option');
+            option.text = select2_opts[i];
+            option.value = select2_opts[i];
+            select2.appendChild(option);
+        }
+        select2_changed();
+    }
+}
+
+function select2_changed() {
+    let opt_value = select2.value;
+    let select3_opts = menu2_menu3[opt_value];
+    if (typeof select3_opts == "undefined") {
+        select3.style.display = 'none';
+    } else {
+        select3.style.display = 'block';
+        while (select3.firstChild) {
+            select3.removeChild(select3.firstChild);
+        }
+        for (let i = 0; i < select3_opts.length; i++) {
+            let option = document.createElement('option');
+            option.text = select3_opts[i];
+            option.value = select3_opts[i];
+            select3.appendChild(option);
         }
     }
+}
 
-    tab_name = btn.innerHTML;
-    // console.log(tab_name);
+function change_tab(btn) {
+    tagging_selections_reset();
+    generate_options_1(btn.dataset.value);
+
+    current_tab = btn.innerHTML;
 }
 
 btn_commit.addEventListener('click', click_commit);
 file_selector.addEventListener('change', file_selected);
+select1.addEventListener('change', select1_changed);
+select2.addEventListener('change', select2_changed);
 
 var stories_json;
 
@@ -53,11 +107,11 @@ function get_story_data() {
         })
         .then(function (myJson) {
             stories_json = myJson;
-            get_file_list();
+            generate_file_list();
         });
 }
 
-function get_file_list() {
+function generate_file_list() {
     for (let i = 0; i < stories_json.length; i++) {
         let option = document.createElement('option');
         option.text = stories_json[i].file_name;
@@ -99,3 +153,56 @@ function doc2btn(tok_list) {
 }
 
 get_story_data();
+
+var ERE_menu1 = {
+    "Entity": ["Per", "Org", "Loc", "Fac", "Veh", "Wea"],
+    "Relation": [""],
+    "Event": ["Life", "Movement", "Transaction", "Business", "Conflict", "Contact", "Personnel", "Justice"]
+}
+
+var menu1_menu2 = {
+    "Life": ["Be-Born", "Marry", "Divorce", "Injure", "Die"],
+    "Movement": ["Transport"],
+    "Transaction": ["Transfer-Ownership", "Transfer-Money"],
+    "Business": ["Start-Org", "Merge-Org", "Declare-Bankruptcy", "End-Org"],
+    "Conflict": ["Attack", "Demonstrate"],
+    "Personnel": ["Start-Position", "End-Position", "Nominate", "Elect"],
+    "Justice": ["Arrest-Jail", "Release-Parole", "Trial-Hearing", "Charge-Indict", "Sue", "Convict", "Sentence", "Fine", "Execute", "Extradite", "Acquit", "Pardon", "Appeal"],
+    "Contact": ["Meet", "Phone-Write"]
+}
+
+var menu2_menu3 = {
+    "Be-Born": ["Person", "Time", "Place"],
+    "Marry": ["Person", "Time", "Place"],
+    "Divorce": ["Person", "Time", "Place"],
+    "Injure": ["Agent", "Victim", "Instrument", "Time", "Place"],
+    "Die": ["Agent", "Victim", "Instrument", "Time", "Place"],
+    "Transport": ["Agent", "Transporter", "Artifact", "Vehicle", "Origin", "Destinat", "Price", "Time"],
+    "Transfer-Ownership": ["Buyer", "Seller", "Beneficiary", "Artifact", "Price", "Time", "Place"],
+    "Transfer-Money": ["Giver", "Recipient", "Beneficiary", "Money", "Time", "Place"],
+    "Start-Org": ["Agent", "Org", "Time", "Place"],
+    "Merge-Org": ["Org", "Time", "Place"],
+    "Declare-Bankruptcy": ["Org", "Time", "Place"],
+    "End-Org": ["Org", "Time", "Place"],
+    "Attack": ["Attacker", "Target", "Instrument", "Time", "Place"],
+    "Demonstrate": ["Entity", "Time", "Place"],
+    "Start-Position": ["Person", "Entity", "Position", "Time", "Place"],
+    "End-Position": ["Person", "Entity", "Position", "Time", "Place"],
+    "Nominate": ["Person", "Agent", "Position", "Time", "Place"],
+    "Elect": ["Person", "Entity", "Position", "Time", "Place"],
+    "Arrest-Jail": ["Person", "Agent", "Crime", "Time", "Duration", "Place"],
+    "Release-Parole": ["Person", "Entity", "Crime", "Time", "Place"],
+    "Trial-Hearing": ["Defendant", "Prosecutor", "Adjudicator", "Crime", "Time", "Place"],
+    "Charge-Indict": ["Defendant", "Prosecutor", "Adjudicator", "Crime", "Time", "Place"],
+    "Sue": ["Plaintiff", "Defendant", "Adjudicator", "Crime", "Time", "Place"],
+    "Convict": ["Defendant", "Adjudicator", "Crime", "Time", "Place"],
+    "Sentence": ["Sentence", "Defendant", "Adjudicator", "Crime", "Time", "Place"],
+    "Fine": ["Entity", "Adjudicator", "Money", "Crime", "Time", "Place"],
+    "Execute": ["Person", "Agent", "Crime", "Time", "Place"],
+    "Extradite": ["Agent", "Person", "Destination", "Origin", "Time", "Crime"],
+    "Acquit": ["Defendant", "Adjudicator", "Crime", "Time", "Place"],
+    "Pardon": ["Defendant", "Adjudicator", "Crime", "Time", "Place"],
+    "Appeal": ["Defendant", "Prosecutor", "Adjudicator", "Crime", "Time", "Place"],
+    "Meet": ["Entity", "Time", "Duration", "Place"],
+    "Phone-Write": ["Entity", "Time", "Duration", "Place"]
+}
